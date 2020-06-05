de092e09024030af105ba9601b3b447073c5822f
velhappy55 <velhappy55@gmail.com>
2020-06-06 00:46:29 +0530

    Updates

---
+++ precache-manifest.06593e25b7fce6efbbd991e68bab2c21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61920fe8b5edac88eabe6b4625f6cb40",
+    "url": "/simple/index.html"
+  },
+  {
+    "revision": "2cfc6eaffce722f21cee",
+    "url": "/simple/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8fd64ae6d523ddc42703",
+    "url": "/simple/static/js/2.34d87c38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple/static/js/2.34d87c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cfc6eaffce722f21cee",
+    "url": "/simple/static/js/main.558e1f22.chunk.js"
+  },
+  {
+    "revision": "f5f4ede4a92b57195d94",
+    "url": "/simple/static/js/runtime-main.e74ae0b6.js"
+  },
+  {
+    "revision": "f6a3099516648ff051190200bec18527",
+    "url": "/simple/static/media/banner_4.f6a30995.jpg"
+  }
+]);
